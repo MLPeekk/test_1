ffd8ff5d32f7c8219855c73ae8b9d62229ebe960
MLPeekk <160306818+MLPeekk@users.noreply.github.com>
2024-02-26 18:14:05 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
         System.out.println("Hello and welcome!");
-        System.out.println(8888);
+        System.out.println(8887);
         System.out.println(9999);
 
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
@@ -13,7 +13,7 @@ public class Main {
 
             // Press Shift+F9 to start debugging your code. We have set one breakpoint
             // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+            System.out.println("i === " + i);
         }
     }
 }
