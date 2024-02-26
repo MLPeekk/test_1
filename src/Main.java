15ed4ceaf2f4ac8fb4a32ae28e408447082bf709
Aleksey Altukhov <avaltukhov7@mail.ru>
2024-02-26 18:30:37 +0300

    some change 5

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,7 @@ public class Main {
         System.out.println("Hello and welcome!");
         System.out.println(8887);
         System.out.println(9999);
+        System.out.println("OK");
 
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
